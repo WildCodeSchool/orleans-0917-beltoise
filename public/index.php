ef0f04a8df6d0e00d293d717cbdf97df9ade49e1
Matthieu Caradet <Matthieu.caradet@gmail.com>
2017-10-04 17:54:16 +0200

    Navbar et fichiers bootstrap

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+include 'header.php';
+include 'navbar.php';
+
+// Include n'arrête pas en cas d'erreur, si vous voulez stoper le chargement: utiliser require.
+?>
+
+
+
+<?php include 'footer.php'; ?>
+
+
