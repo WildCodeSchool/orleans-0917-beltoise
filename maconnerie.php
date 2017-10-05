b2d94937234cee394cdb220224b5f5e0269843e9
Maxime <xm.mx@laposte.net>
2017-10-05 10:15:44 +0200

    maçonnerie

---
+++ maconnerie.php
@@ -0,0 +1,54 @@
+
+<section id="maconnerie">
+    <div class="row separationImg">
+    </div>
+    <h2>Maçonnerie</h2>
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1 col-xs-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. </p>
+            <h3>Nos prestations</h3>
+            <ul>
+                <li>Prestation 1</li>
+                <li>Prestation 2</li>
+                <li>Prestation 3</li>
+                <li>Prestation 4</li>
+                <li>Prestation 5</li>
+            </ul>
+        </div>
+        <!-- carrousel -->
+        <div class="col-md-5 col col-xs-12">
+
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="http://via.placeholder.com/300x300" alt="image 1">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://via.placeholder.com/600x300" alt="image 2">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://via.placeholder.com/400x400" alt="image 3">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+        </div> <!-- fin carrousel -->
+    </div>
+</section>
+
+
