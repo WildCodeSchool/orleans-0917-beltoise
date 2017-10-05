f5b670a17668a9562b3fd5b979b77c7120f57d74
Unknown <chrispichon38@gmail.com>
2017-10-05 13:49:52 +0200

    Création réalisations écologiques

---
+++ public/header.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set('UTC');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="Page Index">
+    <meta name="author" content="Paul DOS SANTOS">
+	<title>Bootstrap template starter</title>
+
+	<!-- Bootstrap core CSS -->
+	<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"> -->
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<!-- Font Awesome -->
+	<link href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<!-- Custom styles for this template -->
+	<link href="../assets/css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+<div class="container-fluid">
+
+
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+include 'header.php';
+
+// include '../src/functions.php';
+// include '../connect_bdd.php';	
+// $bdd = mysqli_connect(SERVEUR, USER, PASSWORD, DATABASE);
+// Include n'arrête pas en cas d'erreur, si vous voulez stoper le chargement: utiliser require.
+?>
+
+<?php
+include 'renoveco.php';
+?>
+
+<?php
+include 'footer.php';
+?>
