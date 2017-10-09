143a0db3e50c602b8cc7bf1a99bb4587377c68b5
Matthieu Caradet <Matthieu.caradet@gmail.com>
2017-10-09 15:08:21 +0200

    modifs CSS &  indentation

--- public/renoveco.php
+++ public/renoveco.php
@@ -3,93 +3,77 @@ include 'header.php';
 ?>
 
 
-<section>
+    <section>
+        <div class="realeco title">
+            <h2>Réalisations Ecologiques</h2>
+        </div>
 
-    <div class = "row realeco">
+        <div class="row realeco">
 
-        <div class = "col-md-3">
-            <h2>Realisation 1</h2>
-            <img src="http://via.placeholder.com/200x200">
+            <div class="col-md-3">
+                <h2>Realisation 1</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Realisation 2</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
+
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Realisation 3</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
         </div>
 
-        <div class = "col-md-3 col-md-offset-1">
-            <h2>Realisation 2</h2>
-            <img src="http://via.placeholder.com/200x200">
+        <div class="row realeco">
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            <div class="col-md-3">
+                <h2>Realisation 4</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
-        </div>
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Realisation 5</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
-        <div class = "col-md-3 col-md-offset-1">
-            <h2>Realisation 3</h2>
-            <img src="http://via.placeholder.com/200x200">
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
-
-        </div>
-
-    </div>
-
-    <div class = "row realeco">
-
-        <div class = "col-md-3">
-            <h2>Realisation 4</h2>
-            <img src="http://via.placeholder.com/200x200">
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Realisation 6</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
         </div>
 
-        <div class = "col-md-3 col-md-offset-1">
-            <h2>Realisation 5</h2>
-            <img src="http://via.placeholder.com/200x200">
+        <div class="row realeco">
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            <div class="col-md-3">
+                <h2>Realisation 7</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
-        </div>
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Realisation 8</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
-        <div class = "col-md-3 col-md-offset-1">
-            <h2>Realisation 6</h2>
-            <img src="http://via.placeholder.com/200x200">
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Realisation 9</h2>
+                <img src="http://via.placeholder.com/200x200">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
+            </div>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
-
-        </div>
-
-    </div>
-
-    <div class = "row realeco">
-
-        <div class = "col-md-3">
-            <h2>Realisation 7</h2>
-            <img src="http://via.placeholder.com/200x200">
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
-
-        </div>
-
-        <div class = "col-md-3 col-md-offset-1">
-            <h2>Realisation 8</h2>
-            <img src="http://via.placeholder.com/200x200">
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
-
-        </div>
-
-        <div class = "col-md-3 col-md-offset-1">
-            <h2>Realisation 9</h2>
-            <img src="http://via.placeholder.com/200x200">
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus</p>
-
-        </div>
-
-    </div>
-
-</section >
+        </div><!--end div row-->
+    </section>
 
 <?php
 include 'footer.php';
