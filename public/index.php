0a70a8aec46f9dda16ccf35142840bcb9db4d1c7
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-26 12:13:58 +0200

    hotfix

--- public/index.php
+++ public/index.php
@@ -7,22 +7,17 @@ use Beltoise\Controller\AdminController;
 
 
 // Routeur basique, necessite une url index.php?route=xxx
-    $route = $_GET['route'];
+$route = $_GET['route'];
 // On appelle une methode d'un controlleur en fonction de la route saisie en URL
 if ($route == 'admin') {
-
     $controller = new AdminController();
-    echo  $controller->showAllAction();
-
+    echo $controller->showAllAction();
 } elseif ($route == 'home') {
-
     $controller = new HomeController();
     echo $controller->showAllAction();
-
-}elseif ($route == 'deleteRenovation') {
-
+} elseif ($route == 'deleteRenovation') {
     $controller = new AdminController();
-    echo  $controller->deleteRenovationAction();
-}else {
+    echo $controller->deleteRenovationAction();
+} else {
     echo 'La page n\'existe pas';
 }
