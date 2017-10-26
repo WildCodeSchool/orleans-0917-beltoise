41d6dced8d0e1065c1fdc997b0ab5f4a98539a4d
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-26 16:22:10 +0200

    Merge branch 'sprint-04_back-delete-slider-certifs' into sprint-04_back-add-slider-certif

--- public/index.php
+++ public/index.php
@@ -10,19 +10,24 @@ use Beltoise\Controller\AdminController;
 // Routeur basique, necessite une url index.php?route=xxx
 $route = $_GET['route'];
 // On appelle une methode d'un controlleur en fonction de la route saisie en URL
-if ($route == 'admin') {
-    $controller = new AdminController();
-    echo $controller->showAllAction();
-} elseif ($route == 'home') {
+if ($route == 'home') {
     $controller = new HomeController();
+    echo $controller->showAllAction();
+} elseif ($route == 'admin') {
+    $controller = new AdminController();
     echo $controller->showAllAction();
 } elseif ($route == 'deleteRenovation') {
     $controller = new AdminController();
     echo $controller->deleteRenovationAction();
-} elseif ($route == 'deleteEcoPlatrerie') {
+} elseif ($route == 'deleteRealisation') {
     $controller = new AdminController();
     echo $controller->deleteRealisationAction();
+} elseif ($route == 'deleteCertification') {
+    $controller = new AdminController();
+    echo $controller->deleteCertificationAction();
+} elseif ($route == 'deleteSlide') {
+    $controller = new AdminController();
+    echo $controller->deleteSlideAction();
 } else {
     echo 'La page n\'existe pas';
 }
-
