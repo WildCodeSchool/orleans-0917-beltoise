73f3a2d7f9dff982abfa784e22285bd484257b78
Unknown <chrispichon38@gmail.com>
2017-10-31 10:37:49 +0100

    Upload image accueil

--- src/Controller/ImageAccueilController.php
+++ src/Controller/ImageAccueilController.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kris
- * Date: 30/10/17
- * Time: 11:15
- */
+
+namespace Beltoise\Controller;
+
+
+use Beltoise\Service\UploadImageManager;
+
+class ImageAccueilController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function showAdminImageAccueil()
+    {
+        $uploadErrors = [];
+
+        if (!empty($_POST)) {
+            $uploadImageManager = new UploadImageManager();
+            $uploadErrors = $uploadImageManager->imageReplace($_FILES, 'imageAccueil.jpg');
+
+            if (empty($uploadErrors)) {
+                header('Location: index.php?route=adminImageAccueil');
+                exit;
+            }
+        }
+
+        return $this->twig->render('Admin/adminImageAccueil.html.twig', [
+            'uploadErrors' => $uploadErrors,
+        ]);
+    }
+}
