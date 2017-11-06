78fd334b4e9e8ec7550627cf5254dff49a321a7b
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-11-06 17:34:19 +0100

    hotfix messages d'erreur

--- src/Model/EntityManager.php
+++ src/Model/EntityManager.php
@@ -14,8 +14,8 @@ class EntityManager
     const UPLOAD_SIZELIMIT = '2000000';
     const PHPERRORTAB = [
         0 => "Aucune erreur détectée.",
-        1 => "L'image est trop lourde.(2 Mo maximum)",
-        2 => "L'image est trop lourde.(2 Mo maximum)",
+        1 => "L'image est trop lourde. (2 Mo maximum)",
+        2 => "L'image est trop lourde. (2 Mo maximum)",
         3 => "Le fichier n'a été que partiellement téléchargé.",
         4 => "Aucun fichier n'a été téléchargé.",
         6 => "Un dossier temporaire est manquant, contactez l'administrateur du site.",
